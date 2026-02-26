eff4761dbb2901edfa0e6c160e516f60fc772ce9
jujudubo25 <jujudubo25@gmail.com>
2026-02-26 09:55:11 -0600

    finished 2.1

--- scripts/Database_setup.R
+++ scripts/Database_setup.R
@@ -45,4 +45,53 @@ dbWriteTable(
 )
 dbListTables(con)
   
+set.seed(123)
+individuals_clinic <- individuals %>%
+  mutate(
+    clinic_id = sample(
+      x = c(1,2),
+      size=n(),
+      replace = TRUE
+    )
+  )
+
+dbWriteTable(
+  conn = con,
+  name = "individuals_clinic",
+  value = individuals_clinic,
+  overWrite = TRUE
+)
+
+q_clinic_create <- "
+CREATE TABLE IF NOT EXISTS clinic_summary (
+  clinic_id INTEGER PRIMARY KEY,
+  clinic_name TEXT,
+  county TEXT,
+  total_patients INTEGER,
+  diabetes_patients INTEGER,
+  hypertension_patients INTEGER
+);
+"
+dbExecute(con, q_clinic_create)
+
+
+
+q_clinic_insert <- "
+INSERT INTO clinic_summary(
+clinic_id, clinic_name, county, total_patients, diabetes_patients, hypertension_patients
+)
+
+VALUES
+(1, 'Downtown Health Center', 'Orleans', 1000, 150, 300),
+(2, 'River Parish Clinic', 'Jefferson', 800, 120, 300)
+ON CONFLICT(clinic_id) DO NOTHING;
+"
+dbExecute(con, q_clinic_insert)
+dbGetQuery(con, "SELECT * FROM clinic_summary;")
+
+dbListTables(con)
+dbListFields(con, "clinic_summary")
+dbListFields(con, "county_prevalence")
+dbListFields(con, "individuals")
+dbListFields(con, "individuals_clinic")
 
